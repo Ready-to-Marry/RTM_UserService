b970510f082f04a93a4322a91b2ca56ee72d5bf9
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-04-29 11:06:04 +0900

    init project

---
+++ UserService/src/main/java/ready_to_marry/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package ready_to_marry.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ UserService/src/test/java/ready_to_marry/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ready_to_marry.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
