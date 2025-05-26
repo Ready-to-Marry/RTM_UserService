d80bb7f95945350e8a6b5d2fe89b830d742a9435
DanidanaCDY <lucy2315.ce@gmail.com>
2025-05-26 10:32:01 +0900

    remove: 테스트용 컴포넌트 삭제 (TestController, TestRepository, Users)

--- UserService/src/main/java/ready_to_marry/userservice/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package ready_to_marry.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ready_to_marry.userservice.entity.user.Users;
-
-@Repository
-public interface TestRepository extends JpaRepository<Users, Long> {
-}
